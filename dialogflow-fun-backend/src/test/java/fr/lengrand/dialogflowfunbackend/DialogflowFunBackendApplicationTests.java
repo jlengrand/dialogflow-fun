afdecdab75de7769d71b2c03662f153f64f262e4
Julien Lengrand-Lambert <julien@lengrand.fr>
2019-10-18 15:00:39 +0200

    Introduce backend project and rename dialogflow agent

---
+++ dialogflow-fun-backend/src/test/java/fr/lengrand/dialogflowfunbackend/DialogflowFunBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.lengrand.dialogflowfunbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DialogflowFunBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
